ec36d490c8d67f0293daf528bbf648ed5ecaf9eb
kottaharshitha <144656746+kottaharshitha@users.noreply.github.com>
2024-01-08 12:09:12 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -5,18 +5,18 @@ st.header("Types of Cats")
 col1,col2,col3,col4 = st.columns(4)
 with col1:
   st.subheader("Persian Cat")
-  st.image("./cat1.jpeg",caption="Persion Cat",width=300,use_column_width=True)
+  st.image("./cat1.jpeg",caption="Persion Cat",width=500,use_column_width=True)
   st.write("Persian cats are cute")
 with col2:
   st.subheader("Ragdoll Cat")
-  st.image("./cat2.jpeg",caption="Ragdoll Cat",width=300,use_column_width=True)
+  st.image("./cat2.jpeg",caption="Ragdoll Cat",width=500,use_column_width=True)
   st.write("Ragdoll cats are white in colour")
 with col3:
-  st.subheader("Ragdoll Cat")
-  st.image("./cat2.jpeg",caption="Ragdoll Cat",width=300,use_column_width=True)
-  st.write("Ragdoll cats are white in colour")
+  st.subheader("House Cat")
+  st.image("./cat3.jpeg",caption="house Cat",width=500,use_column_width=True)
+  st.write("house cats are very calm")
 with col4:
-  st.subheader("Ragdoll Cat")
-  st.image("./cat2.jpeg",caption="Ragdoll Cat",width=300,use_column_width=True)
-  st.write("Ragdoll cats are white in colour")
+  st.subheader("Random Cat")
+  st.image("./cat4.jpeg",caption="Random Cat",width=500,use_column_width=True)
+  st.write("Random cats are different in nature")
   
