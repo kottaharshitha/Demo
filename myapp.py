370a60646aff7044bd7b445c0f8f89cea893dc0e
kottaharshitha <144656746+kottaharshitha@users.noreply.github.com>
2024-01-08 14:02:24 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 st.set_page_config(page_title='cats')
-st.header("Types of Cats")
+st.markdown("<h3 style='color: blue,font-size: 30px;'>Types of Cats</h3>", unsafe_allow_html=True)
 
 col1,col2,col3,col4 = st.columns(4)
 with col1:
@@ -9,15 +9,15 @@ with col1:
   st.image("./cat1.jpeg",caption="Persion Cat",width=500,use_column_width=True)
   st.write("Persian cats are cute")
 with col2:
-  st.markdown("<h3 style='color: blue;'>Ragdoll Cat</h3>", unsafe_allow_html=True)
+  st.markdown("<h3 style='color: pink;'>Ragdoll Cat</h3>", unsafe_allow_html=True)
   st.image("./cat2.jpeg",caption="Ragdoll Cat",width=500,use_column_width=True)
   st.write("Ragdoll cats are white in colour")
 with col3:
-  st.markdown("<h3 style='color: blue;'>House Cat</h3>", unsafe_allow_html=True)
+  st.markdown("<h3 style='color: red;'>House Cat</h3>", unsafe_allow_html=True)
   st.image("./cat3.jpeg",caption="house Cat",width=500,use_column_width=True)
   st.write("house cats are very calm")
 with col4:
-  st.markdown("<h3 style='color: blue;'>Random Cat</h3>", unsafe_allow_html=True)
+  st.markdown("<h3 style='color: green;'>Random Cat</h3>", unsafe_allow_html=True)
   st.image("./cat4.jpeg",caption="Random Cat",width=500,use_column_width=True)
   st.write("Random cats are different in nature")
   
