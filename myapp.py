13ac6037dbb670af8941ae54a40167364335634a
kottaharshitha <144656746+kottaharshitha@users.noreply.github.com>
2024-01-08 11:44:38 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -6,8 +6,8 @@ col1,col2 = st.columns(2)
 with col1:
   st.subheader(Persian Cat")
   st.image("./cat1.jpeg",caption="Persion Cat",width=300,use_column_width=True)
-  st.write("Persian cats are cute")
+  st.write("Persian cats are cute and nice")
 with col2:
   st.subheader("Ragdoll Cat")
   st.image("./cat2.jpeg",caption="Ragdoll Cat",width=300,use_column_width=True)
-  st.write("Ragdoll cats are proud")
+  st.write("Ragdoll cats are white in colour")
