d36c52e60fa8ea7c45a6bfe6d6f90b643ee539a1
kottaharshitha <144656746+kottaharshitha@users.noreply.github.com>
2024-01-08 14:03:57 +0530

    Update myapp.py

--- myapp.py
+++ myapp.py
@@ -1,7 +1,7 @@
 
 import streamlit as st
 st.set_page_config(page_title='cats')
-st.markdown("<h3 style='color: blue,font-size: 30px;'>Types of Cats</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='color: Yellow,font-size: 60px;'>Types of Cats</h3>", unsafe_allow_html=True)
 
 col1,col2,col3,col4 = st.columns(4)
 with col1:
@@ -9,7 +9,7 @@ with col1:
   st.image("./cat1.jpeg",caption="Persion Cat",width=500,use_column_width=True)
   st.write("Persian cats are cute")
 with col2:
-  st.markdown("<h3 style='color: pink;'>Ragdoll Cat</h3>", unsafe_allow_html=True)
+  st.markdown("<h3 style='color: orange;'>Ragdoll Cat</h3>", unsafe_allow_html=True)
   st.image("./cat2.jpeg",caption="Ragdoll Cat",width=500,use_column_width=True)
   st.write("Ragdoll cats are white in colour")
 with col3:
